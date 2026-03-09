dbfb5706a9a29363e3aad6539456dfd38927624a
Терещенко Степан <tereschenkostep@gmail.com>
2026-03-09 11:10:14 +0500

    init commit

---
+++ src/main/java/org/example/yourordercore/YourorderCoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.yourordercore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourorderCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourorderCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/yourordercore/YourorderCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.yourordercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourorderCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
